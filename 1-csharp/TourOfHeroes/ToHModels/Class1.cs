161b56c1b190ac456f75902722b122cdca7b275c
mariellenolasco <marielle.nolasco@revature.com>
2021-02-16 12:03:56 -0800

    Added initial structure for ToH App

---
+++ 1-csharp/TourOfHeroes/ToHModels/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ToHModels
+{
+    public class Class1
+    {
+    }
+}
---
+++ 1-csharp/TourOfHeroes/ToHUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToHUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
