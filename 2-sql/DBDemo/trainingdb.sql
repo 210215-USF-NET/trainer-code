48ec8c62332652d89692b2d5457632a05b29d5e4
mariellenolasco <marielle.nolasco@revature.com>
2021-02-24 08:02:49 -0800

    activity db structure

--- 2-sql/DBDemo/trainingdb.sql
+++ 2-sql/DBDemo/trainingdb.sql
@@ -22,3 +22,10 @@ create table batch
 	associateID int references associates(id),
 	trainerID int references trainers (id)
 );
+
+insert into associates (associateName, associateLocale, revaPoints) values 
+('Joaquin', 'PA', -10), ('Madeline', 'WA', 10), ('Jacob', 'FL', 0), ('Michael', 'AZ', 20), ('Janel', 'AZ', 50), ('Angeleen', 'TX', 30), ('Warren', 'NH', 40);
+insert into trainers (trainerName, campus, caffeineLevel) values
+('Marielle', 'USF', 5), ('Pushpinder', 'UTA', 50), ('Nick', 'UTA', 75), ('Mark', 'WVU', 16), ('Fred', 'UTA', 25);
+insert into batch (associateID, trainerID) values
+(3,3), (1,1), (2,1), (4,1), (6,1), (7,1);
