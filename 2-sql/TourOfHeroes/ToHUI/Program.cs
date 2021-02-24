86ac54159dab1213cf981e42021a1a2a41af638d
mariellenolasco <marielle.nolasco@revature.com>
2021-02-24 12:08:03 -0800

    Phase 2 of DB first

--- 2-sql/TourOfHeroes/ToHUI/Program.cs
+++ 2-sql/TourOfHeroes/ToHUI/Program.cs
@@ -1,12 +1,31 @@
 ﻿using ToHBL;
 using ToHDL;
+using ToHDL.Entities;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
 namespace ToHUI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            IMenu menu = new HeroMenu(new HeroBL(new HeroRepoFile()));
+            //get the config file
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            //setting up db connection
+            string connectionString = configuration.GetConnectionString("HeroDB");
+            DbContextOptions<HeroDBContext> options = new DbContextOptionsBuilder<HeroDBContext>()
+            .UseSqlServer(connectionString)
+            .Options;
+
+            //using statement used to dispose of the context when its no longer used 
+            using var context = new HeroDBContext(options);
+
+            IMenu menu = new HeroMenu(new HeroBL(new HeroRepoDB(context, new HeroMapper())));
             menu.Start();
         }
     }
