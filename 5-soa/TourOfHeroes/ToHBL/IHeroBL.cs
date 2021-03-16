e8fecd8c84f849bc1494411e3a9e87cdb17056c9
mariellenolasco <marielle.nolasco@revature.com>
2021-03-16 09:41:26 -0700

    ToHREST version

---
+++ 5-soa/TourOfHeroes/ToHBL/IHeroBL.cs
@@ -0,0 +1,15 @@
+using ToHModels;
+using System.Collections.Generic;
+namespace ToHBL
+{
+    public interface IHeroBL
+    {
+        List<Hero> GetHeroes();
+        Hero AddHero(Hero newHero);
+
+        Hero GetHeroByName(string name);
+        Hero DeleteHero(Hero hero2BDeleted);
+
+        Hero UpdateHero(Hero hero2BUpdated);
+    }
+}
